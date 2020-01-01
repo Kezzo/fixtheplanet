e72f7983b4a616a1ead549dbba2cfb7d4dfe48cc
Kezzo <jon_jelinski@t-online.de>
2020-01-01 19:32:46 +0200

    Implement getting issues from github

--- backend/src/main.go
+++ backend/src/main.go
@@ -1,14 +1,57 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Pong!")
+		w.Write([]byte("It's alive!"))
+	})
+
+	http.HandleFunc("/gitdata", func(w http.ResponseWriter, r *http.Request) {
+
+		fileData, err := ioutil.ReadFile("../queries/get-issues.gql")
+
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(500)
+		}
+
+		mapData := map[string]string{
+			"query":     string(fileData),
+			"variables": "",
+		}
+		requestBody, err := json.Marshal(mapData)
+
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(500)
+		}
+
+		req, err := http.NewRequest("POST", "https://api.github.com/graphql", bytes.NewBuffer(requestBody))
+		req.Header.Add("Authorization", "bearer "+os.Getenv("GITHUB-TOKEN"))
+		client := &http.Client{}
+		resp, err := client.Do(req)
+
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(500)
+		}
+
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+
+		if err != nil {
+			log.Fatal(err)
+			w.WriteHeader(500)
+		}
+
+		w.Write(bodyBytes)
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
