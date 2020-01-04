4d8ad31d7809848988d1085c64157132e96ecd02
Kezzo <jon_jelinski@t-online.de>
2020-01-04 12:36:44 +0200

    Create issues table when db starts

--- backend/local-db/create.sql
+++ backend/local-db/create.sql
@@ -1,3 +1,10 @@
 DROP DATABASE IF EXISTS `fixtheplanet`;
 CREATE DATABASE fixtheplanet;
 USE fixtheplanet;
+CREATE TABLE issues(
+    issue_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    repo VARCHAR(255) NOT NULL,
+    language VARCHAR(30) NULL,
+    url VARCHAR(512) NOT NULL,
+    platform VARCHAR(30) NOT NULL);
