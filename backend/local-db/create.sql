2c984ba01cb7173f9a83aa1f99bd50dc2d40d651
Kezzo <jon_jelinski@t-online.de>
2020-01-04 18:24:31 +0200

    Implement getting issues from database

--- backend/local-db/create.sql
+++ backend/local-db/create.sql
@@ -5,5 +5,4 @@ CREATE TABLE issues(
     issue_id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     repo VARCHAR(255) NOT NULL,
-    language VARCHAR(30) NULL,
-    platform VARCHAR(30) NOT NULL);
+    language VARCHAR(30) NULL);
--- backend/src/main.go
+++ backend/src/main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"database/sql"
+	"encoding/json"
 	"log"
 	"net/http"
 	"strconv"
 
 	"github.com/Kezzo/fixtheplanet/src/common"
 )
+
+// Issue ..
+type Issue struct {
+	Title    string
+	Repo     string
+	Language string
+}
+
+// IssueResponse ..
+type IssueResponse struct {
+	Issues []Issue
+}
 
 func main() {
 	log.Println("Starting server...")
@@ -23,6 +37,38 @@ func main() {
 		w.Write([]byte("It's alive!"))
 	})
 
+	http.HandleFunc("/issues", func(w http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query()
+		languages := query["language"]
+
+		for _, lang := range languages {
+			if lang == "" {
+				http.Error(w, "Url Param 'language' is empty", http.StatusBadRequest)
+				return
+			}
+		}
+
+		issues, err := queryIssues(db, languages)
+
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, err.Error(), 500)
+		}
+
+		resp := IssueResponse{
+			Issues: issues,
+		}
+
+		respBytes, err := json.Marshal(resp)
+
+		if err != nil {
+			log.Println(err.Error())
+			http.Error(w, err.Error(), 500)
+		}
+
+		w.Write(respBytes)
+	})
+
 	http.HandleFunc("/gitdata", func(w http.ResponseWriter, r *http.Request) {
 		resp, err := common.GetIssuesFromGithub()
 
@@ -31,30 +77,100 @@ func main() {
 			http.Error(w, err.Error(), 500)
 		}
 
-		insert, err := db.SQLDB.Prepare("INSERT INTO issues VALUES( ?, ?, ?, ?, ? )") // ? = placeholder
+		inserted, err := insertIssuesIntoDB(db, resp)
+
 		if err != nil {
-			panic(err.Error())
+			log.Println(err.Error())
+			http.Error(w, err.Error(), 500)
 		}
 
-		defer insert.Close()
-
-		count := 0
-		for _, repo := range resp.Data.Search.Edges {
-			for _, issue := range repo.Node.Issues.Edges {
-				_, err = insert.Exec(nil, issue.Node.Title, repo.Node.NameWithOwner, repo.Node.PrimaryLanguage.Name, "github")
-				if err != nil {
-					log.Println("Error inserting issue: " + err.Error())
-				} else {
-					count++
-				}
-			}
-		}
-
-		respString := "Inserted " + strconv.Itoa(count) + " issues into database"
+		respString := "Inserted " + strconv.Itoa(inserted) + " issues into database"
 
 		log.Println(respString)
 		w.Write([]byte(respString))
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func insertIssuesIntoDB(db *common.Database, resp *common.GithubResponse) (int, error) {
+	stmt, err := db.SQLDB.Prepare("INSERT INTO issues VALUES( ?, ?, ?, ? )")
+	if err != nil {
+		return 0, err
+	}
+
+	defer stmt.Close()
+
+	count := 0
+	for _, repo := range resp.Data.Search.Edges {
+		for _, issue := range repo.Node.Issues.Edges {
+			_, err = stmt.Exec(nil, issue.Node.Title, repo.Node.NameWithOwner, repo.Node.PrimaryLanguage.Name)
+			if err != nil {
+				log.Println("Error inserting issue: " + err.Error())
+			} else {
+				count++
+			}
+		}
+	}
+
+	return count, nil
+}
+
+func queryIssues(db *common.Database, languages []string) ([]Issue, error) {
+
+	queryString := "SELECT * FROM issues"
+
+	for i := 0; i < len(languages); i++ {
+		if i == 0 {
+			queryString += " WHERE language = ?"
+		} else {
+			queryString += " OR language = ?"
+		}
+	}
+
+	queryString += " ORDER BY RAND() LIMIT 20" // TODO: Implement proper random pagination
+
+	stmt, err := db.SQLDB.Prepare(queryString)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer stmt.Close()
+
+	var rows *sql.Rows
+	if languages != nil && len(languages) > 0 {
+		rows, err = stmt.Query(languages)
+	} else {
+		rows, err = stmt.Query()
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	issues := make([]Issue, 20)
+	issueID := ""
+
+	defer rows.Close()
+	count := 0
+	for rows.Next() {
+		issue := Issue{}
+		err = rows.Scan(&issueID, &issue.Title, &issue.Repo, &issue.Language)
+
+		if err != nil {
+			return nil, err
+		}
+
+		issues[count] = issue
+		count++
+	}
+
+	err = rows.Err()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return issues, nil
 }
